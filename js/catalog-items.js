789dc1269baca90c208d444f66e59e4ca9410464
Eugene Yalandaev <yalandaev@gmail.com>
2018-06-28 19:41:22 +0400

    Слайдер с сервисами и горем пополам

--- js/catalog-items.js
+++ js/catalog-items.js
@@ -2,15 +2,15 @@
 var buyButtons = document.getElementsByClassName('catalog-item-actions__buy');
 for (var i = 0; i < buyButtons.length; i++) {
     var item = buyButtons[i];
-    item.addEventListener("focusin", onActionButtonFocus);
-    item.addEventListener("focusout", onActionButtonFocusOut);
+    item.addEventListener('focusin', onActionButtonFocus);
+    item.addEventListener('focusout', onActionButtonFocusOut);
 }  
 // Подписка кнопок "В закладки" на события focusin и focusout
 var bookmarkButtons = document.getElementsByClassName('catalog-item-actions__bookmark');
 for (var i = 0; i < bookmarkButtons.length; i++) {
     var item = bookmarkButtons[i];
-    item.addEventListener("focusin", onActionButtonFocus);
-    item.addEventListener("focusout", onActionButtonFocusOut);
+    item.addEventListener('focusin', onActionButtonFocus);
+    item.addEventListener('focusout', onActionButtonFocusOut);
 }  
 
 /**
@@ -20,7 +20,7 @@ for (var i = 0; i < bookmarkButtons.length; i++) {
  */
 function onActionButtonFocus(event) {
     var actionsContainer = event.target.parentNode.parentNode;
-    actionsContainer.setAttribute("style", "z-index: 12");
+    actionsContainer.setAttribute('style', 'z-index: 12');
 }
 /**
  * Обработчик события при focusout кнопки "Купить" или "В закладки"
@@ -29,5 +29,5 @@ function onActionButtonFocus(event) {
  */
 function onActionButtonFocusOut(event) {
     var actionsContainer = event.target.parentNode.parentNode;
-    actionsContainer.removeAttribute("style");
+    actionsContainer.removeAttribute('style');
 }
--- js/slider-catalog.js
+++ js/slider-catalog.js
@@ -20,16 +20,16 @@ function setSlideByIndex(n) {
 
 function showSlideByIndex(n) {
   var i;
-  var slides = document.getElementsByClassName("slider-item");
-  var dots = document.getElementsByClassName("slider__btn-dot");
+  var slides = document.getElementsByClassName('slider-item');
+  var dots = document.getElementsByClassName('slider__btn-dot');
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+      slides[i].style.display = 'none'; 
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" slider__btn-dot--active", "");
+      dots[i].className = dots[i].className.replace(' slider__btn-dot--active', '');
   }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " slider__btn-dot--active";
+  slides[slideIndex-1].style.display = 'block'; 
+  dots[slideIndex-1].className += ' slider__btn-dot--active';
 }
