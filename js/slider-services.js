71635b80ca7a0b59c0e7926028033e410f06f9dc
Eugene Yalandaev <yalandaev@gmail.com>
2018-06-28 19:47:27 +0400

    Удалил ненужные классы из разметки слайдера сервисов

--- js/slider-services.js
+++ js/slider-services.js
@@ -9,15 +9,15 @@ function setServiceByIndex(n) {
 function showServiceByIndex(n) {
   var i;
   var slides = document.getElementsByClassName('service-item');
-  var dots = document.getElementsByClassName('tab main-services__tab');
+  var dots = document.getElementsByClassName('main-services__tab');
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = 'none'; 
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(' tab main-services__tab--active', '');
+      dots[i].className = dots[i].className.replace(' main-services__tab--active', '');
   }
   slides[slideIndex-1].style.display = 'block'; 
-  dots[slideIndex-1].className += ' tab main-services__tab--active';
+  dots[slideIndex-1].className += ' main-services__tab--active';
 }
