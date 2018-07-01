cd367b33541d27ad52389bf98fb75b2f752510ec
Eugene Yalandaev <yalandaev@gmail.com>
2018-07-02 02:55:53 +0400

    Критерий Д23. Проведена базовая оптимизация: минифицированы стили и скрипты

--- js/catalog.js
+++ js/catalog.js
@@ -1,3 +1,38 @@
+// Подписка кнопок "Купить" на события focusin и focusout
+var buyButtons = document.getElementsByClassName('catalog-item-actions__buy');
+for (var i = 0; i < buyButtons.length; i++) {
+    var item = buyButtons[i];
+    item.addEventListener('focusin', onActionButtonFocus);
+    item.addEventListener('focusout', onActionButtonFocusOut);
+}  
+// Подписка кнопок "В закладки" на события focusin и focusout
+var bookmarkButtons = document.getElementsByClassName('catalog-item-actions__bookmark');
+for (var i = 0; i < bookmarkButtons.length; i++) {
+    var item = bookmarkButtons[i];
+    item.addEventListener('focusin', onActionButtonFocus);
+    item.addEventListener('focusout', onActionButtonFocusOut);
+}  
+
+/**
+ * Обработчик события при focusin кнопки "Купить" или "В закладки"
+ *
+ * @param event - событие OnFocusIn
+ */
+function onActionButtonFocus(event) {
+    var actionsContainer = event.target.parentNode.parentNode;
+    actionsContainer.setAttribute('style', 'z-index: 12');
+}
+/**
+ * Обработчик события при focusout кнопки "Купить" или "В закладки"
+ *
+ * @param event - событие OnFocusOut
+ */
+function onActionButtonFocusOut(event) {
+    var actionsContainer = event.target.parentNode.parentNode;
+    actionsContainer.removeAttribute('style');
+}
+
+// Подписка кнопок "Купить" на событие нажатия
 var productAddedModal = document.querySelector('.modal-product-added');
 var close = productAddedModal.querySelector('.modal-close');
 var orderButton = productAddedModal.querySelector('.modal-product-added__order');
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// Write us
+//// Write us
 var writeUsLink = document.querySelector(".main-contacts__btn");
 var writeUsModal = document.querySelector(".modal-write-us");
 var closeWriteUs = writeUsModal.querySelector(".modal-close");
@@ -15,7 +15,7 @@ closeWriteUs.addEventListener("click", function (evt) {
     writeUsLink.focus();
 });
 
-// Map
+//// Map
 var mapLink = document.querySelector(".map");
 var mapModal = document.querySelector(".modal-map");
 var closeMap = mapModal.querySelector(".modal-close");
@@ -33,12 +33,74 @@ closeMap.addEventListener("click", function (evt) {
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (writeUsModal.classList.contains("modal-show")) {
-        writeUsModal.classList.remove("modal-show");
-      }
-      if (mapModal.classList.contains("modal-show")) {
-        mapModal.classList.remove("modal-show");
-      }
+        evt.preventDefault();
+        if (writeUsModal.classList.contains("modal-show")) {
+            writeUsModal.classList.remove("modal-show");
+        }
+        if (mapModal.classList.contains("modal-show")) {
+            mapModal.classList.remove("modal-show");
+        }
     }
-  });
+});
+
+//// Slider Catalog
+var currentCatalogSliderIndex = 1;
+showSlideByIndex(currentCatalogSliderIndex);
+
+function setPrevSlide() {
+    rollSlides(-1);
+}
+function setNextSlide() {
+    rollSlides(1);
+}
+
+// Next/previous controls
+function rollSlides(n) {
+    showSlideByIndex(currentCatalogSliderIndex += n);
+}
+
+// Thumbnail image controls
+function setSlideByIndex(n) {
+    showSlideByIndex(currentCatalogSliderIndex = n);
+}
+
+function showSlideByIndex(n) {
+  var i;
+  var slides = document.getElementsByClassName('slider-item');
+  var dots = document.getElementsByClassName('slider__btn-dot');
+  if (n > slides.length) {currentCatalogSliderIndex = 1} 
+  if (n < 1) {currentCatalogSliderIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none'; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' slider__btn-dot--active', '');
+  }
+  slides[currentCatalogSliderIndex-1].style.display = 'block'; 
+  dots[currentCatalogSliderIndex-1].className += ' slider__btn-dot--active';
+}
+
+//// Slider Services
+var currentServiceSliderIndex = 1;
+showServiceByIndex(currentServiceSliderIndex);
+
+// Thumbnail image controls
+function setServiceByIndex(n) {
+    showServiceByIndex(currentServiceSliderIndex = n);
+}
+
+function showServiceByIndex(n) {
+  var i;
+  var slides = document.getElementsByClassName('service-item');
+  var dots = document.getElementsByClassName('main-services__tab');
+  if (n > slides.length) {currentServiceSliderIndex = 1} 
+  if (n < 1) {currentServiceSliderIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none'; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' main-services__tab--active', '');
+  }
+  slides[currentServiceSliderIndex-1].style.display = 'block'; 
+  dots[currentServiceSliderIndex-1].className += ' main-services__tab--active';
+}
