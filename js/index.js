478c3f10ee4e7e8a16b78d1f743d898ab5f9997c
Eugene Yalandaev <yalandaev@gmail.com>
2018-07-02 09:47:36 +0400

    Поправил кавычки в js

--- js/index.js
+++ js/index.js
@@ -1,44 +1,44 @@
 //// Write us
-var writeUsLink = document.querySelector(".main-contacts__btn");
-var writeUsModal = document.querySelector(".modal-write-us");
-var closeWriteUs = writeUsModal.querySelector(".modal-close");
-var writeUsNameInput = writeUsModal.querySelector("[name=name]");
+var writeUsLink = document.querySelector('.main-contacts__btn');
+var writeUsModal = document.querySelector('.modal-write-us');
+var closeWriteUs = writeUsModal.querySelector('.modal-close');
+var writeUsNameInput = writeUsModal.querySelector('[name=name]');
 
-writeUsLink.addEventListener("click", function (evt) {
+writeUsLink.addEventListener('click', function (evt) {
     evt.preventDefault();
-    writeUsModal.classList.add("modal-show");
+    writeUsModal.classList.add('modal-show');
     writeUsNameInput.focus();
 });   
-closeWriteUs.addEventListener("click", function (evt) {
+closeWriteUs.addEventListener('click', function (evt) {
     evt.preventDefault();
-    writeUsModal.classList.remove("modal-show");
+    writeUsModal.classList.remove('modal-show');
     writeUsLink.focus();
 });
 
 //// Map
-var mapLink = document.querySelector(".map");
-var mapModal = document.querySelector(".modal-map");
-var closeMap = mapModal.querySelector(".modal-close");
+var mapLink = document.querySelector('.map');
+var mapModal = document.querySelector('.modal-map');
+var closeMap = mapModal.querySelector('.modal-close');
 
-mapLink.addEventListener("click", function (evt) {
+mapLink.addEventListener('click', function (evt) {
     evt.preventDefault();
-    mapModal.classList.add("modal-show");
+    mapModal.classList.add('modal-show');
     closeMap.focus();
 });   
-closeMap.addEventListener("click", function (evt) {
+closeMap.addEventListener('click', function (evt) {
     evt.preventDefault();
-    mapModal.classList.remove("modal-show");
+    mapModal.classList.remove('modal-show');
 });
 
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
     if (evt.keyCode === 27) {
         evt.preventDefault();
-        if (writeUsModal.classList.contains("modal-show")) {
-            writeUsModal.classList.remove("modal-show");
+        if (writeUsModal.classList.contains('modal-show')) {
+            writeUsModal.classList.remove('modal-show');
         }
-        if (mapModal.classList.contains("modal-show")) {
-            mapModal.classList.remove("modal-show");
+        if (mapModal.classList.contains('modal-show')) {
+            mapModal.classList.remove('modal-show');
         }
     }
 });
